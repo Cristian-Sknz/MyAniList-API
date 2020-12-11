e69c26aecf072cf0e956ea18be6e5b9411a7d7a0
Cristian "SknZ" Ferreira <61399406+Cristian-Sknz@users.noreply.github.com>
2020-12-11 17:59:27 -0300

    [feature] Adicionados getName() no MediaType.java

--- src/main/java/me/skiincraft/mal/entity/objects/MediaType.java
+++ src/main/java/me/skiincraft/mal/entity/objects/MediaType.java
@@ -4,8 +4,18 @@ import java.util.Arrays;
 
 public enum MediaType {
 
-	OVA, TV, Special, ONA, Movie, Manga, Novel, Visual;
-	
+	OVA("Ova"), TV("TV"), Special("Special"), ONA("ONA"), Movie("Movie"), Manga("Manga"), Novel("Light Novel"), Visual("Visual Novel");
+
+	private final String name;
+
+	MediaType(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
 	public static MediaType getMediaType(String string) {
 		return Arrays.stream(values()).filter(o -> o.name().equalsIgnoreCase(string)).findAny().orElse(null);
 	}
